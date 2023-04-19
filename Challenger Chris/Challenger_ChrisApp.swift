caeccab856d541ef4a2f861f9951266effdd9438
JuanNeiverth <polli.neiverth@gmail.com>
2023-04-19 11:08:41 -0500

    Initial Commit

---
+++ Challenger Chris/Challenger_ChrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenger_ChrisApp.swift
+//  Challenger Chris
+//
+//  Created by Juan on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenger_ChrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
